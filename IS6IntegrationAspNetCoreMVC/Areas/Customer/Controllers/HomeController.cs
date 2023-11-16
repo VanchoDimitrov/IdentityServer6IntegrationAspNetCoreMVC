da190c053db04a769df73c53a89e381326db6f33
Vancho Dimitrov <vancho.dimitrov@outlook.com>
2023-11-16 21:05:18 +0100

    registering dbcontext in the program.cs, creating area Customer, CSS

---
+++ IS6IntegrationAspNetCoreMVC/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IS6IntegrationAspNetCoreMVC.Areas.Customer.Controllers
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this._logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var person = new PersonViewModel()
+            {
+                FirstName = "Ana",
+                LastName = "Dimitrova",
+                Email = "ana.dimitrova@gmail.com",
+            };
+
+            return View(person);
+        }
+    }
+}
--- IS6IntegrationAspNetCoreMVC/Program.cs
+++ IS6IntegrationAspNetCoreMVC/Program.cs
@@ -1,7 +1,21 @@
+using Integration.DataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// ApplicationDbContext registration.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("DefaultConnection"),
+         sqlServerOptionsAction: sqlOptions =>
+         {
+             sqlOptions.MigrationsAssembly("IS6IntegrationAspNetCoreMVC");
+         });
+});
 
 var app = builder.Build();
 
@@ -22,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
